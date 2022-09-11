b6453b8550f4ea4fcec045766cdcedc750ea9e2f
qq8351176 <1045882688@qq.com>
2022-09-11 11:22:20 +0800

    asm-learn

---
+++ p28.asm
@@ -0,0 +1,16 @@
+assume cs:codesg
+codesg segment
+    dw 0123h,0456H,0789H,0abcH,0defH,0fedH,0cbaH,0987H
+
+  start:mov bx,0
+    mov ax,0
+    mov cx,8
+
+  s:add ax,cs:[bx]
+    add bx,2
+    loop s
+
+    mov ax,4c00h
+    int 21h
+codesg ends
+end start
